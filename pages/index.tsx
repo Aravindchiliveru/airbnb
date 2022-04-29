dc46c09ad4fe4ce1cbf164bb8d6cbe117e75a219
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-04-29 16:19:23 +0530

    change 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,61 +11,58 @@ import { ReactChild, ReactFragment, ReactPortal } from 'react'
 
 
 
-const Home: NextPage = (props) => {
-  return (
-    <div className="">
-      <Head>
-        <title>Airbnb clone by Aravind</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-        <Header />
-        <Banner />
+function Home({ exploreData, cardsData } : { exploreData : any, cardsData : any }) {
+    return (
+        <div className="">
+            <Head>
+                <title>Airbnb clone by Aravind</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <Banner />
 
-        <main className='max-w-7xl mx-auto px-8 sm:px-16'>
-            <section className='pt-6'>
-                <h2 className='text-4xl font-semibold pb-5'>
-                    Explore Nearby
-                </h2>
-                {/* Pull from server */}
-                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+                <section className='pt-6'>
+                    <h2 className='text-4xl font-semibold pb-5'>
+                        Explore Nearby
+                    </h2>
+                    {/* Pull from server */}
+                    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 
-                {props.exploreData.map(({img, distance, location}) => (
-                    <SmallCard
-                        key={img}
-                        img = {img}
-                        distance = {distance}
-                        location = {location}
-                    />
-                ))}
+                        {exploreData?.map(({ img, distance, location }: { img: any; distance: any; location: any} ) => (
+                            <SmallCard
+                                key={img}
+                                img={img}
+                                distance={distance}
+                                location={location} />
+                        ))}
 
-                </div>
-            </section>
-                    
-            <section>
-                <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+                    </div>
+                </section>
+
+                <section>
+                    <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
                     <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
-                    {props.cardsData?.map(({img, title}) => (
-                        <MediumCard 
-                            key={img}
-                            img = {img}
-                            title = {title} 
-                            />
+                        {cardsData?.map(({ img, title }: { img: any; title: any} ) => (
+                            <MediumCard
+                                key={img}
+                                img={img}
+                                title={title} />
                         ))}
                     </div>
-                   
-            </section>
-            
-            <LargeCard 
-                img= "https://links.papareact.com/4cj"
-                        title= "The Greatest Outdoors"
-                        description= "Wishlists curated by Airbnb."
-                        buttonText="Get Inspired"
-            />
-        </main>
-        <Footer />
 
-    </div>
-  )
+                </section>
+
+                <LargeCard
+                    img="https://links.papareact.com/4cj"
+                    title="The Greatest Outdoors"
+                    description="Wishlists curated by Airbnb."
+                    buttonText="Get Inspired" />
+            </main>
+            <Footer />
+
+        </div>
+    )
 }
 
 export async function getStaticProps() {
